145ce9370f4aa3984056c97c2a54be10c5cca53f
himanshu-thakur-7 <himan7781@gmail.com>
2021-05-22 17:39:27 +0530

    search bar set up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 function Header() {
   return (
     <header>
@@ -15,9 +20,16 @@ function Header() {
           />
         </div>
         {/* Search  */}
-        <div>
-            <input type="text" />
+        <div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer  bg-yellow-400 hover:bg-yellow-500'>
+          <input
+            className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4'
+            type='text'
+          />
+          <SearchIcon className='h-12 p-4' />
         </div>
+        {/* Right */}
+
+        
       </div>
       {/* Bottom Nav */}
       <div></div>
