dc11e317a6901843d1e3814fa3082e08af772df7
himanshu-thakur-7 <himan7781@gmail.com>
2021-05-22 16:59:56 +0530

    Initial setup completed plus top bar

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+function Header() {
+  return (
+    <header>
+      {/* Top Nav */}
+      <div className='flex item-center bg-amazon_blue p-1 flex-grow py-2'>
+        <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
+          <Image
+            src='https://links.papareact.com/f90'
+            width={150}
+            height={40}
+            objectFit='contain'
+            className='cursor-pointer'
+          />
+        </div>
+        {/* Search  */}
+        <div>
+            <input type="text" />
+        </div>
+      </div>
+      {/* Bottom Nav */}
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
