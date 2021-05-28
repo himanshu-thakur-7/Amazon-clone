79e3f0bbb324d85cd9250a5bbd540daecfc26147
himanshu-thakur-7 <himan7781@gmail.com>
2021-05-29 01:43:46 +0530

    UI completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,33 @@
 import Head from "next/head";
 import Header from "../components/Header";
-
-export default function Home() {
+import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
+export default function Home({ products }) {
   return (
-    <div>
+    <div className='bg-gray-200'>
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-          <Header/>
+      <Header />
+      <main className='max-w-screen-2xl mx-auto'>
+        {/* banner */}
+        <Banner />
+        {/* Product feed component */}
+        <ProductFeed products={products} />
+      </main>
     </div>
   );
 }
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      products,
+    },
+  };
+}
+
+// Get >> https://fakestoreapi.com/products
